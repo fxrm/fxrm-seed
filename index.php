dd3b12d04d057255f502381ab05927dd69719dfe
Nick Matantsev <nick@sigmaxis.com>
2013-07-09 23:09:16 +0000

    Refactored form helper

--- index.php
+++ index.php
@@ -10,17 +10,15 @@ $service = $ctx->createService('\\TodoApp\\LoggedInApplication');
 
 $app = $service->createInstance();
 
+$setEmailForm = new \Fxrm\Action\Form($service, 'api.php/setEmail', array('session' => $app->getSession()), 'setEmail');
+
 var_dump($app->findAllUsers());
 
 $mustache = new Mustache_Engine(array(
     'strict_callables' => true,
     'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/views'),
     'helpers' => array(
-        'setEmailForm' => new \Fxrm\Action\Form($service, 'api.php/setEmail', array(
-            'session' => $app->getSession()
-        ), 'setEmail', array(
-            'email' => $app->getEmail()
-        ))
+        'setEmailForm' => new \Fxrm\Mustache\FormHelper($setEmailForm, array('email' => $app->getEmail()))
     )
 ));
 
--- login.php
+++ login.php
@@ -8,11 +8,18 @@ $ctx = include(__DIR__ . '/context.php');
 
 $service = $ctx->createService('\\TodoApp\\Application');
 
+$loginForm = new \Fxrm\Action\Form($service, 'api.php/login', array(), 'login');
+
+if ($loginForm->getSuccessStatus()) {
+    header('Location: ' . 'index.php?session=' . urlencode($loginForm->getSuccessValue()));
+    return;
+}
+
 $mustache = new Mustache_Engine(array(
     'strict_callables' => true,
     'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/views'),
     'helpers' => array(
-        'loginForm' => new \Fxrm\Action\Form($service, 'api.php/login', array(), 'login', array())
+        'loginForm' => new \Fxrm\Mustache\FormHelper($loginForm)
     )
 ));
 
