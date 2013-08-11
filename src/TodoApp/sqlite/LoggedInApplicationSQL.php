f0078100702d3e4cc9658f0eea1a8c990f5b32c2
Nick Matantsev <nick@sigmaxis.com>
2013-08-11 21:39:14 +0000

    Cleanup

--- src/TodoApp/sqlite/LoggedInApplicationSQL.php
+++ src/TodoApp/sqlite/LoggedInApplicationSQL.php
@@ -3,7 +3,7 @@
 namespace TodoApp\sqlite;
 
 class LoggedInApplicationSQL {
-    const findAllUsers = '
+    const queryAllUsers = '
         select u.ROWID as id, u.name, u.email, s.createdTime as lastLoginTime
         from User u left join Session s on(s.ROWID = u.lastSessionId)
     ';
