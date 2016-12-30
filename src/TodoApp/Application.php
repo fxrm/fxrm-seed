76692ff2c32613a32ec42616286cef8b671a6d50
Nick Matantsev <nick.m@myplanetdigital.com>
2016-12-30 16:32:11 -0500

    Some old changes

--- src/TodoApp/Application.php
+++ src/TodoApp/Application.php
@@ -15,15 +15,22 @@ abstract class Application {
      */
     abstract function findUserByEmail($email);
 
+    /**
+     * @return SessionId
+     */
+    abstract function findSessionByKey($key);
+
+    abstract function getSessionUserId(SessionId $session);
+
     abstract function setUserLastSession(UserId $userId, SessionId $lastSessionId);
 
     abstract function initializeSession(SessionId $session, $key, UserId $userId, $createdTime);
 
-    function checkUserPassword(UserId $userId, $password) {
+    private function checkUserPassword(UserId $userId, $password) {
         return $this->getUserPasswordHash($userId) === '$md5$' . md5($password);
     }
 
-    function login($email, $password) {
+    function lodgin($email, $password) {
         $userId = $this->findUserByEmail($email);
 
         if ( ! $userId || ! $this->checkUserPassword($userId, $password)) {
@@ -39,6 +46,19 @@ abstract class Application {
         $this->setUserLastSession($userId, $sessionId);
 
         return $sessionKey;
+    }
+
+    function getCurrentUserDetails($sessionKey) {
+        // @todo check session expiry
+        $sessionId = $this->findSessionByKey($sessionKey);
+        $userId = $sessionId ? $this->getSessionUserId($sessionId) : null;
+
+        if ( ! $userId) {
+            throw new InvalidSessionException();
+        }
+
+        // @todo always update session expiry
+        return array('email' => 'yeah!'); // @todo this
     }
 }
 
---
+++ src/TodoApp/InvalidSessionException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace TodoApp;
+
+class InvalidSessionException extends ApplicationException {}
+
+?>
